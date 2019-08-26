480703df04efbf9cabcbed903a227386ac558bf3
Alex <dev.alex.88@gmail.com>
2019-08-26 16:15:25 +0700

    update

--- Test Suites/Run Test Suites in parallel.ts
+++ Test Suites/Run Test Suites in parallel.ts
@@ -3,7 +3,7 @@
    <description></description>
    <name>Run Test Suites in parallel</name>
    <tag></tag>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>3</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
