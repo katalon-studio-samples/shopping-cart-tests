cd0b69eabc6a2ac2f00741348262fe6db23cf9bf
Alex <dev.alex.88@gmail.com>
2019-08-27 16:05:20 +0700

    rename test suites

--- Test Suites/Run All Test Suites.ts
+++ Test Suites/Run All Test Suites.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>Run Test Suites in parallel</name>
+   <name>Run All Test Suites</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>3</maxConcurrentInstances>
