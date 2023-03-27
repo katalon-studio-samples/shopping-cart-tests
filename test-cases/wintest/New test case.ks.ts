4ae7e6106e4c448136cd50134002f52b2d4a4f9a
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-03-27 11:13:07 +0700

    1st commit

---
+++ test-cases/wintest/New test case.ks.ts
@@ -0,0 +1,6 @@
+import katalon from "katalon";
+
+export default katalon.testCase("New Test Case", async ({ local, web }) => {
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.closeBrowser();
+});
