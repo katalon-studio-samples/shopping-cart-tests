a2fb6ddd68701a68f0acc8029277507ac1be8bf5
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-04-25 18:43:20 +0700

    2nd commit

--- test-suites/test.ts
+++ test-suites/test.ts
@@ -1,13 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>test</name>
-    <testSuiteGuid>59a0c95c-caab-49ff-a5e2-f37df0072d21</testSuiteGuid>
+    <testSuiteGuid>938a8268-be1e-4521-a7e2-d548a329e15a</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/wintest/New test case</testCaseId>
-        <guid>8d905e28-c0f6-4141-b449-68fc5e4e31a4</guid>
-    </testCaseLink>
-    <testCaseLink>
-        <testCaseId>test-cases/wintest/New test case - 1</testCaseId>
-        <guid>be2a1095-4ffe-46de-ac6f-5b8b044249e5</guid>
+        <guid>4a523114-1cd7-4d33-be09-7ec39125d773</guid>
     </testCaseLink>
 </TestSuiteEntity>
