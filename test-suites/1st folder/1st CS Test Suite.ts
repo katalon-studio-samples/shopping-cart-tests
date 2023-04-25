066c5ededc3656690f77a2d5dee996f96ea9959c
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-04-25 21:34:03 +0700

    First commit

---
+++ test-suites/1st folder/1st CS Test Suite.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>1st CS Test Suite</name>
+    <testSuiteGuid>8eec1258-38e3-485b-aba2-040c34ccf710</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/wintest/New test case</testCaseId>
+        <guid>33f1ee7a-afff-438f-95a7-19722f4cdfea</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/wintest/New test case - 1</testCaseId>
+        <guid>1d2ddde6-c520-40ce-8686-9eccc2fb5a62</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/test-folder/1st CS test case</testCaseId>
+        <guid>e4a307c3-8b81-4485-96f5-92866a507769</guid>
+    </testCaseLink>
+</TestSuiteEntity>
