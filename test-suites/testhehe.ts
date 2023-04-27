5c1c3e5bd5a96626d5bd59fac376e845482453ee
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-04-27 10:45:18 +0700

    hehe

---
+++ test-suites/testhehe.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>testhehe</name>
+    <testSuiteGuid>c142e92f-933c-4e28-90e2-0255cca6a543</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/wintest/New test case</testCaseId>
+        <guid>f9b33528-5269-4983-895f-a44ad42ec375</guid>
+    </testCaseLink>
+</TestSuiteEntity>
