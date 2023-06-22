4eecf336b47e53046a000429245c92bda9bbc11b
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-06-22 10:11:37 +0700

    Katalon: Update test case New test case (1) - Win test - 22062023 in test-cases/test

--- test-cases/test/New test case (1) - Win test - 22062023.ks.ts
+++ test-cases/test/New test case (1) - Win test - 22062023.ks.ts
@@ -3,37 +3,5 @@ import katalon from "@katalon-studio/cloud-test";
 export default katalon.testCase("New Test Case", async ({ local, web }) => {
   await new Promise((r) => setTimeout(r, 0));
   await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
-  await web.click({
-    type: "Web",
-    id: "b44788ab-a095-4a2d-8386-b7e3719214ef",
-    attributes: {
-      id: "btn-make-appointment",
-      href: "./profile.php#login",
-      class: "btn btn-dark btn-lg",
-    },
-    childIndex: 4,
-    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
-    name: "a - Make Appointment",
-    selectors: [
-      {
-        id: "fefbe928-b3de-4c0a-9b22-f0fab2335a6b",
-        type: "CSS",
-        value: "#btn-make-appointment",
-        isDefault: true,
-      },
-      {
-        id: "af594b5d-6b1c-4950-888d-7258a11a1940",
-        type: "Attribute",
-        value: null,
-        isDefault: true,
-      },
-    ],
-    selectorType: "CSS",
-    tag: "a",
-    text: "Make Appointment",
-    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
-    parentIframe: null,
-    shadowRoot: null,
-  });
   await web.closeBrowser();
 });
