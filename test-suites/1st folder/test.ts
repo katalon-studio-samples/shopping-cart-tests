69ae46f2055111f4e35f95f076be9da24a92ec02
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-07-14 12:08:38 +0700

    nnnn

---
+++ test-suites/1st folder/test.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>test</name>
+    <testSuiteGuid>82967930-6313-4233-a36e-c925b6d4150f</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/wintest/New test case</testCaseId>
+        <guid>c0e2bd90-6014-47af-aa54-c3a00a16ac46</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/wintest/New test case - 1</testCaseId>
+        <guid>fe974c63-4988-4b17-b9a0-189344763cfb</guid>
+    </testCaseLink>
+</TestSuiteEntity>
