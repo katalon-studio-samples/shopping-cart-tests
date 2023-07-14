7eb34383d1895bbf6c0039946c09b1f652919814
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-07-14 12:12:45 +0700

    ádsadasd

---
+++ test-suites/1st folder/ádasdad.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ádasdad</name>
+    <testSuiteGuid>f39e8df6-fb57-4757-b4d5-feff1de2d905</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/test-folder/1st CS test case</testCaseId>
+        <guid>cba6fe82-6519-402c-84b8-daeb187ddc77</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/test-folder/New test case</testCaseId>
+        <guid>f4b9097b-c3ab-441c-9e1e-42c53a861ae6</guid>
+    </testCaseLink>
+</TestSuiteEntity>
