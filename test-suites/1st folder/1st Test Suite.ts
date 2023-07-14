359edf8ade585e5ac0571a3eee9c6cffbae36d89
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-07-14 12:13:12 +0700

    bbbbb

--- test-suites/1st folder/1st Test Suite.ts
+++ test-suites/1st folder/1st Test Suite.ts
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>1st Test Suite</name>
-    <testSuiteGuid>85a6d415-f33c-4cbc-9d87-e7b2b445998a</testSuiteGuid>
+    <testSuiteGuid>963a0165-6d63-42d7-9a79-0f3c2b563692</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/wintest/New test case</testCaseId>
-        <guid>14633834-71c6-4996-b72d-abb6d66f6e14</guid>
+        <guid>e93f7724-0a17-4222-99d7-44c3732fe6e0</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/test-folder/1st CS test case</testCaseId>
-        <guid>84639ee8-eae9-4d21-989f-63f0c040dde2</guid>
+        <guid>c0d0c120-d5b4-417a-9063-cd09e9e389b2</guid>
     </testCaseLink>
 </TestSuiteEntity>
