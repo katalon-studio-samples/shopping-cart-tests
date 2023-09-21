d738546948f1faf29f335df941489e2509a6c12a
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-09-21 09:46:48 +0700

    Delete test-suites/test.ts

--- test-suites/test.ts
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<TestSuiteEntity>
-    <name>test</name>
-    <testSuiteGuid>938a8268-be1e-4521-a7e2-d548a329e15a</testSuiteGuid>
-    <testCaseLink>
-        <testCaseId>test-cases/wintest/New test case</testCaseId>
-        <guid>4a523114-1cd7-4d33-be09-7ec39125d773</guid>
-    </testCaseLink>
-</TestSuiteEntity>
