ffdd7dddbf078b0862fe801937fabb1582a6a16e
trile-katalon <112364759+trile-katalon@users.noreply.github.com>
2023-09-21 09:46:18 +0700

    Delete test-cases/test directory

--- test-cases/test/New test case - Win - Na.ks.ts
+++
@@ -1,7 +0,0 @@
-import katalon from "@katalon-studio/cloud-test";
-
-export default katalon.testCase("New Test Case", async ({ local, web }) => {
-  await new Promise((r) => setTimeout(r, 0));
-  await web.openBrowser("https://katalon-demo-cura.herokuapp.com/");
-  await web.closeBrowser();
-});
